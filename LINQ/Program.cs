cc4cd980f159f90a64df553bdc1ec3d75835a1a0
Rob van der Heijden <rheijdendeveloper@gmail.com>
2022-10-14 11:00:27 +0200

    Add example

---
+++ LINQ/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        private static Random random = new Random();
+        private static int[] ints = new int[] { };
+        private static Stopwatch stopWatch = new Stopwatch();
+      
+        static void Main(string[] args)
+        {
+            Spinner.Start();
+
+            GetIntegers(10000000);
+
+            stopWatch.Reset();
+            LinqExIt();
+
+            stopWatch.Reset();
+            LinqIt();
+                       
+            Spinner.Stop();
+
+            Console.Read();
+        }
+
+        static void LinqIt()
+        {
+            stopWatch.Start();
+
+            //var result = from i in ints
+            //             where i > 500
+            //             select i;
+            var result = from i in ints
+                         where i > 500
+                         select new { id = Guid.NewGuid(), value = i };
+
+            stopWatch.Stop();
+
+
+            Console.WriteLine();
+            Console.WriteLine($"It took {stopWatch.Elapsed.ToString("mm\\:ss\\.ff")} for linq");
+        }
+
+        static void LinqExIt()
+        {
+            stopWatch.Start();
+
+            //var result = ints.Where(i => i > 500).Select(i => i).ToList();
+            
+            //var result = ints.Where(i => i > 500).ToList();
+            
+            var result = ints.Where(i => i > 500).Select(i => new { id = Guid.NewGuid(), value = i }).ToList();
+
+            stopWatch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine($"It took {stopWatch.Elapsed.ToString("mm\\:ss\\.ff")} for linq ex");
+        }
+
+        static void GetIntegers(int numberOfInts)
+        {
+            ints = new int[numberOfInts];
+            for (var i = 0; i < numberOfInts; i++)
+            {
+                ints[i] = random.Next(1, numberOfInts);
+            }
+        }
+    }
+}
---
+++ LINQ/Spinner.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+namespace LINQ
+{
+    public static class Spinner
+    {
+        private static BackgroundWorker spinner = initialiseBackgroundWorker();
+        private static int spinnerPosition = 25;
+        private static int spinWait = 25;
+        private static bool isRunning;
+        public static bool IsRunning { get { return isRunning; } }
+
+        private static BackgroundWorker initialiseBackgroundWorker()
+        {
+            BackgroundWorker obj = new BackgroundWorker();
+            obj.WorkerSupportsCancellation = true;
+
+            obj.DoWork += delegate
+            {
+                spinnerPosition = Console.CursorLeft;
+                while (!obj.CancellationPending)
+                {
+                    char[] spinChars = new char[] { '|', '/', '-', '\\' };
+
+                    foreach (char spinChar in spinChars)
+                    {
+                        Console.CursorLeft = spinnerPosition;
+                        Console.Write(spinChar);
+                        Thread.Sleep(spinWait);
+
+                    }
+
+                }
+
+            };
+
+            return obj;
+        }
+
+        public static void Start()
+        {
+            isRunning = true;
+            if (!spinner.IsBusy)
+                spinner.RunWorkerAsync();
+            else throw new InvalidOperationException("Cannot start spinner whilst spinner is already running");
+        }
+
+        public static void Stop()
+        {
+            spinner.CancelAsync();
+            while (spinner.IsBusy) System.Threading.Thread.Sleep(100);
+            Console.CursorLeft = spinnerPosition;
+            isRunning = false;
+        }
+    }
+}
